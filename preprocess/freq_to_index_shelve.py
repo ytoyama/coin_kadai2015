8409981554daa0fb3d25783eccaa73969206efa8
Yota Toyama <ytoyama11@gmail.com>
2015-06-24 11:18:32 +0900

    word freq analyzer with persistent db added

---
+++ preprocess/freq_to_index_shelve.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python2
+
+from __future__ import print_function
+import nltk
+import sys
+import shelve
+
+assert len(sys.argv) == 2
+
+with open(sys.argv[1]) as f:
+  wordCounter = {}
+  wordIndex = shelve.open('word_freq.db')
+  index = 1
+  for sentence in map(nltk.tokenize.word_tokenize,
+            nltk.tokenize.sent_tokenize(f.read())):
+    for word in sentence:
+      if word not in wordIndex:
+        wordIndex[word] = index
+        index += 1
+      if word in wordCounter:
+        wordCounter[word] += 1
+      else:
+        wordCounter[word] = 1
+
+for word, count in wordCounter.items():
+  print(wordIndex[word], ':', word, ':', count)
+
+wordIndex.close()

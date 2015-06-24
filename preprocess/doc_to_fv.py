b89a327b9ae2638b45fd75d2d6b8107aa11d9369
Yota Toyama <ytoyama11@gmail.com>
2015-06-24 20:06:51 +0900

    doc_to_fv.py's index bug fixed

--- preprocess/doc_to_fv.py
+++ preprocess/doc_to_fv.py
@@ -9,21 +9,20 @@ import operator
 assert len(sys.argv) == 2
 
 with open(sys.argv[1]) as f:
-  wordCounter = {}
+  wordCount = shelve.open('word_count.db')
   wordIndex = shelve.open('word_freq.db')
-  index = 1
   for sentence in map(nltk.tokenize.word_tokenize,
             nltk.tokenize.sent_tokenize(f.read())):
     for word in sentence:
       if word not in wordIndex:
-        wordIndex[word] = index
-        index += 1
-      if word in wordCounter:
-        wordCounter[word] += 1
+        wordIndex[word] = len(wordIndex) + 1
+      if word in wordCount:
+        wordCount[word] += 1
       else:
-        wordCounter[word] = 1
+        wordCount[word] = 1
 
+assert len(wordCount) == len(wordIndex)
 for word, index in sorted(wordIndex.items(), key=operator.itemgetter(1)):
-  print("{}:{} {}".format(index, wordCounter[word], word))
+  print("{}:{} {}".format(index, wordCount[word], word))
 
 wordIndex.close()

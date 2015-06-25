0d411c1fd49ee74cd4b244c49684a62abff85ad3
Yota Toyama <ytoyama11@gmail.com>
2015-06-25 14:30:20 +0900

    doc_to_fv.py is fixed against my misunderstanding output format

--- preprocess/doc_to_fv.py
+++ preprocess/doc_to_fv.py
@@ -8,22 +8,26 @@ import operator
 
 assert len(sys.argv) == 2
 
+wordCount = shelve.open('word_count.db')
+wordIndex = shelve.open('word_index.db')
+
 with open(sys.argv[1]) as f:
-  wordCount = shelve.open('word_count.db')
-  wordIndex = shelve.open('word_freq.db')
   for sentence in map(nltk.tokenize.word_tokenize,
             nltk.tokenize.sent_tokenize(f.read())):
     for word in sentence:
-      if word not in wordIndex:
-        wordIndex[word] = len(wordIndex) + 1
+      assert len(wordCount) == len(wordIndex)
       if word in wordCount:
+        assert word in wordIndex
         wordCount[word] += 1
       else:
+        assert word not in wordIndex
         wordCount[word] = 1
+        wordIndex[word] = len(wordIndex) + 1
 
-assert len(wordCount) == len(wordIndex)
-for word, index in sorted(wordIndex.items(), key=operator.itemgetter(1)):
-  print("{}:{} {}".format(index, wordCount[word], word))
+print(" ".join(
+    ["{}:{}".format(index, wordCount[word])
+    for word, index in sorted(wordIndex.items(),
+    key=operator.itemgetter(1))]))
 
 wordIndex.close()
 wordCount.close()

45fba9783e3337eb3c7aaed06757944532d79abd
Yota Toyama <ytoyama11@gmail.com>
2015-06-25 16:43:48 +0900

    dir_to_data.py can be executed for each label

--- preprocess/dir_to_data.py
+++ preprocess/dir_to_data.py
@@ -8,6 +8,14 @@ import os.path
 import shelve
 import operator
 
+
+DEBUG = True
+
+def debug(*x):
+  if DEBUG:
+    print(*x, file=sys.stderr)
+
+
 if len(sys.argv) != 3:
   print("usage: {} <label> <directory>".format(sys.argv[0]))
   exit(1)
@@ -19,12 +27,13 @@ TARGET_DIR = sys.argv[2]
 assert os.path.isdir(TARGET_DIR)
 
 # initialize db
-wordCount = shelve.open('word_count.db')
-wordIndex = shelve.open('word_index.db')
+wordCount = shelve.open('word_count_{}.db'.format(LABEL))
+wordIndex = shelve.open('word_index_{}.db'.format(LABEL))
 
 for item in map(lambda path: os.path.join(TARGET_DIR, path),
     os.listdir(TARGET_DIR)):
   if os.path.isfile(item):
+    debug("file:", item)
     with open(item, 'r') as f:
       for sentence in map(nltk.tokenize.word_tokenize,
           nltk.tokenize.sent_tokenize(f.read())):
@@ -44,5 +53,6 @@ print(LABEL, " ".join(
     key=operator.itemgetter(1))]))
 
 # finalize db
+assert len(wordCount) == len(wordIndex), "when finalizing db"
 wordCount.close()
 wordIndex.close()

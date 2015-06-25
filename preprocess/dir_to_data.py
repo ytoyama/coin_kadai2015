506fdfdeef15997a2eb88ed90f2a65e71f3a0bb5
Yota Toyama <ytoyama11@gmail.com>
2015-06-25 14:17:55 +0900

    dir_to_data.py is completed

---
+++ preprocess/dir_to_data.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python2
+
+from __future__ import print_function
+import nltk
+import sys
+import os
+import os.path
+import shelve
+import operator
+
+if len(sys.argv) != 3:
+  print("usage: {} <label> <directory>".format(sys.argv[0]))
+  exit(1)
+
+LABEL = sys.argv[1]
+TARGET_DIR = sys.argv[2]
+
+assert LABEL.isdigit()
+assert os.path.isdir(TARGET_DIR)
+
+# initialize db
+wordCount = shelve.open('word_count.db')
+wordIndex = shelve.open('word_index.db')
+
+for item in map(lambda path: os.path.join(TARGET_DIR, path),
+    os.listdir(TARGET_DIR)):
+  print(item)
+  if os.path.isfile(item):
+    with open(item, 'r') as f:
+      for sentence in map(nltk.tokenize.word_tokenize,
+          nltk.tokenize.sent_tokenize(f.read())):
+        for word in sentence:
+          assert len(wordCount) == len(wordIndex)
+          if word in wordCount:
+            assert word in wordIndex
+            wordCount[word] += 1
+          else:
+            assert word not in wordIndex
+            wordIndex[word] = len(wordIndex) + 1
+            wordCount[word] = 1
+
+for word, index in sorted(wordIndex.items(), key=operator.itemgetter(1)):
+  print("{} {}:{} {}".format(LABEL, index, wordCount[word], word))
+
+# finalize db
+wordCount.close()
+wordIndex.close()

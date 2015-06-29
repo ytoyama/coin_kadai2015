0bbdcd7308891e0322c17a27f087caeb4d8defae
Yota Toyama <ytoyama11@gmail.com>
2015-06-29 17:06:43 +0900

    i'm git but could

--- perceptron/perceptron.py
+++ perceptron/perceptron.py
@@ -3,6 +3,7 @@
 import sys
 import math
 import random
+import copy
 
 
 # debugger
@@ -34,7 +35,8 @@ def read_data(filename):
   with open(filename) as f:
     instances = [read_instance(line)
         for line in f.read().split('\n') if line]
-  return (instances, max(len(instance[1]) for instance in instances))
+  return (instances, max(len(instance[1]) - 1 for instance in instances))
+  # instance[1] - 1 is for max index of data itself (without bias term)
 
 def __add_fv(weight, fv):
   assert len(weight) >= len(fv)
@@ -52,18 +54,22 @@ def __sub_fv(weight, fv):
 def sub_fv(weight, fv, nupdates):
   __sub_fv(weight, [(elem[0], nupdates * elem[1]) for elem in fv])
 
-def mult_fv(wight, fv):
+def mult_fv(weight, fv):
   assert len(weight) >= len(fv)
   return sum(x * y[1] for x, y in zip(weight, fv))
 
-def update_weight(weight, instance, nupdates):
+def update_weight(weight, tmp_weight, instance, nupdates):
   iprod = mult_fv(weight, instance[1])
   if (iprod * instance[0] <= 0 or abs(iprod) < IPROD_THRESHOLD) \
       and instance[0] > 0:
-    add_fv(weight, instance[1], nupdates)
+    #debug("addition performed.")
+    add_fv(weight, instance[1], 1)
+    add_fv(tmp_weight, instance[1], nupdates)
   elif (iprod * instance[0] <= 0 or abs(iprod) < IPROD_THRESHOLD) \
       and instance[0] < 0:
-    sub_fv(weight, instance[1], nupdates)
+    #debug("substruction performed.")
+    sub_fv(weight, instance[1], 1)
+    sub_fv(tmp_weight, instance[1], nupdates)
   else:
     pass
     #debug("weight was not updated.")
@@ -83,7 +89,7 @@ def evaluate(weight, instances):
 
 # main routine
 
-if __name__ == "__main__":
+def main():
   assert len(sys.argv) == 3, \
       "usage: {} <train data> <test data>".format(sys.argv[0])
 
@@ -92,15 +98,29 @@ if __name__ == "__main__":
   # process train data
   train_instances, train_max_index = read_data(sys.argv[1])
 
-  random.shuffle(train_instances)
+
+  #random.shuffle(train_instances)
   weight = [0] * (train_max_index + 1)
-  tmp_weight = weight
+  tmp_weight = copy.deepcopy(weight)
+  sum_weight = copy.deepcopy(weight) # DEBUG
   nupdates = 0
+  #debug(train_instances)
+
   for instance in train_instances:
     nupdates += 1
-    update_weight(weight, instance, 1)
-    update_weight(tmp_weight, instance, nupdates)
+    #debug("nupdates =", nupdates)
+    update_weight(weight, tmp_weight, instance, nupdates)
+    #debug("weight =", weight)
+    #debug("tmp_weight =", tmp_weight)
+    sum_weight = [sum(x) for x in zip(sum_weight, weight)] # DEBUG
+    #debug("sum_weight =", sum_weight)
+
+  sum_weight = [x / (nupdates + 1) for x in sum_weight]
+  ave_weight = averaged_weight(weight, tmp_weight, nupdates)
+  #debug("sum_weight =", sum_weight) # DEBUG
+  #debug("ave_weight =", ave_weight)
   #debug("weight =", weight)
+  #debug("tmp_weight =", tmp_weight)
 
   # process test data
   test_instances, test_max_index = read_data(sys.argv[2])
@@ -117,3 +137,7 @@ if __name__ == "__main__":
   #  x = [1, 1, 1, 1]
   #  update_weight(x, (1, [(3, -2), (2, -2), (1, -2)]))
   #  debug(x)
+
+
+if __name__ == "__main__":
+  main()

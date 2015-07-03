2626f763ce734c39611afcafc040665a8ea0195a
Yota Toyama <ytoyama11@gmail.com>
2015-07-03 18:52:55 +0900

    texts_to_ngram.py completed

---
+++ rakuten/texts_to_ngram.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python2
+# encoding: utf-8
+
+from __future__ import print_function
+import nltk
+import sys
+import gzip
+import MeCab
+import shelve
+
+
+UTF8 = 'utf-8'
+WORD_INDEX_FILE = 'word_index.db'
+
+
+def nGram(numOfGram, kNode):
+  node = kNode
+  nGram = ""
+  for _ in range(numOfGram):
+    if not node.next: # ignore the last meanless word in a sentence
+      break
+    nGram += node.surface
+    node = node.next
+  return nGram
+
+
+def main(*args):
+  if len(args) == 3:
+    N_GRAM = int(args[1])
+    filename = args[2]
+  else:
+    print("usage: {} <number of grams> <filename>".format(args[0]))
+    return
+  del args
+
+  nGramIndex = shelve.open(str(N_GRAM) + '_gram_index.db')
+  fv = {}
+
+  with gzip.open(filename, 'r') as f: # 'rb' or 'rt' for unicode files?
+    jpSentenceTokenizer = nltk.RegexpTokenizer(u'[^！？。]*[！？。]?')
+    sentences = [sentence.encode(UTF8) for sentence
+        in jpSentenceTokenizer.tokenize(f.readline().decode(UTF8)) if sentence]
+
+  tagger = MeCab.Tagger()
+
+  for sentence in sentences:
+    node = tagger.parseToNode(sentence)
+    node = node.next # ignore the first meanless word
+    while node.next: # ignore the last meanless word
+      if nGram(N_GRAM, node) not in nGramIndex:
+        nGramIndex[nGram(N_GRAM, node)] = len(nGramIndex) + 1
+      if nGramIndex[nGram(N_GRAM, node)] in fv:
+        fv[nGramIndex[nGram(N_GRAM, node)]] += 1
+      else:
+        fv[nGramIndex[nGram(N_GRAM, node)]] = 1
+      node = node.next
+  print(fv)
+
+  nGramIndex.close()
+
+
+if __name__ == "__main__":
+  main(*sys.argv)

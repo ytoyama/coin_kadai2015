f0cb95ca006d1cd181cf87532d352aa6cc117149
Yota Toyama <ytoyama11@gmail.com>
2015-07-06 16:17:14 +0900

    csv.sh chaged along with the renewed bias option

--- perceptron/csv.sh
+++ perceptron/csv.sh
@@ -8,7 +8,6 @@ N_TRAIN_INSTANCES=$(echo $(wc -l "$TRAIN_DATA") | cut -f 1 -d ' ')
 N_UPDATE=$(expr $N_TRAIN_INSTANCES \* 8)
 
 UPDATE_CSV=updates.csv
-BIAS_CSV=bias.csv
 MARGIN_CSV=margin.csv
 
 
@@ -33,7 +32,7 @@ do
 
   # create new files
 
-  for file in "$UPDATE_CSV" "$BIAS_CSV" "$MARGIN_CSV"
+  for file in "$UPDATE_CSV" "$MARGIN_CSV"
   do
     : > "$CSV_DIR/${i_iter}_$file"
   done
@@ -49,19 +48,6 @@ do
     echo "i = $num"
     echo "$num,$(python3 perceptron.py -u $(expr $N_TRAIN_INSTANCES \* $num) \
         "$TRAIN_DATA" "$TEST_DATA")" >> "$CSV_DIR/${i_iter}_$UPDATE_CSV"
-  done
-  
-  
-  # bias
-  
-  echo creating "$CSV_DIR/$BIAS_CSV"
-  range=$(seq 0 0.2 3)
-  echo "range =" $range
-  for num in $range
-  do
-    echo "i = $num"
-    echo "$num,$(python3 perceptron.py -b $num -u $N_UPDATE \
-        "$TRAIN_DATA" "$TEST_DATA")" >> "$CSV_DIR/${i_iter}_$BIAS_CSV"
   done
   
   
